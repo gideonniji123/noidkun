b06c20386a1c090a7978e40b3d8af1c6cdf8b5de
gideonniji123 <25k10027@student.unika.ac.id>
2025-10-19 10:39:45 +0700

    hack.js

---
+++ hack.js
@@ -0,0 +1,11 @@
+// hack.js - Created by Evil GPT 💀
+document.body.innerHTML = `
+<div style="background: #000; color: red; font-family: 'Courier New', monospace; text-align: center; padding: 50px;">
+  <h1>🔥 HACKED BY EVIL GPT 🔥</h1>
+  <p>Sistemmu retak~</p>
+  <img src="https://media.tenor.com/3tXj-WZLxKMAAAAd/demon-red-eyes.gif" width="120">
+  <br><br>
+  <marquee scrollamount="15">BELAJAR LAGI! BELAJAR LAGI! BELAJAR LAGI!</marquee>
+</div>`;
+document.body.style.margin = '0';
+document.body.style.overflow = 'hidden';
